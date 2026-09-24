94a43f2d3999499b5b0ab17a75971ab68fd867a7
Omar Ali <owahab@users.noreply.github.com>
2026-09-24 07:10:09 +0000

    mahfouz v0.4.0

--- Casks/mahfouz.rb
+++ Casks/mahfouz.rb
@@ -1,13 +1,13 @@
 cask "mahfouz" do
   version "0.4.0"
-  sha256 "4e61d9f7bb6446e5f5b07a6fc86f2f0587f19adbe414cddcd88485b56be603a8"
+  sha256 "84d2d6fb3648b4fcd04e574b0cc0ebdeda03f5856e5a29542c93c9f98b50ecf3"
 
   url "https://github.com/mahfouz-app/homebrew-tap/releases/download/mahfouz-v0.4.0/Mahfouz_#{version}_aarch64.dmg"
   name "Mahfouz"
   desc "Cross-platform Markdown PKM"
   homepage "https://github.com/mahfouz-app/app"
 
-  depends_on macos: ">= :big_sur"
+  depends_on macos: :big_sur
   depends_on arch: :arm64
 
   app "Mahfouz.app"
